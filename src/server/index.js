103e711ae04bd09f83a92c75b5626eb9b4ebe6ba
Ajay <ajay.bhardwaj@sprinklr.com>
2021-08-03 21:32:06 +0530

    fixed image path and add config to github pages

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ export function fetchData() {
       title: "Cat",
       description:
         "I am a monster cat (Weaown Waeown). Don't come near me. I will not bite you but will roast your insecurities. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elit velit, convallis id consectetur ut, varius et diam. Nam lacus orci, elementum et bibendum ultricies, tincidunt nec lectus. Duis blandit et mi in sodales. Fusce nec purus porttitor, pharetra mi a, accumsan nisi. Integer neque urna, cursus sed ex id, auctor vehicula mi. Donec ac finibus magna. Proin sollicitudin erat sit amet orci tincidunt, facilisis lacinia dolor ornare.",
-      imageURL: "/images/cat.jpeg",
+      imageURL: "images/cat.jpeg",
     },
     {
       id: Math.random().toString().substring(0, 5),
@@ -13,7 +13,7 @@ export function fetchData() {
       title: "Dog",
       description:
         "I am a cool puppy (aow aow). You can play with me or take me to a walk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elit velit, convallis id consectetur ut, varius et diam. Nam lacus orci, elementum et bibendum ultricies, tincidunt nec lectus. Duis blandit et mi in sodales. Fusce nec purus porttitor, pharetra mi a, accumsan nisi. Integer neque urna, cursus sed ex id, auctor vehicula mi. Donec ac finibus magna. Proin sollicitudin erat sit amet orci tincidunt, facilisis lacinia dolor ornare.",
-      imageURL: "/images/dog.jpeg",
+      imageURL: "images/dog.jpeg",
     },
     {
       id: Math.random().toString().substring(0, 5),
@@ -21,7 +21,7 @@ export function fetchData() {
       title: "Horse",
       description:
         "I am a horse (HeHeHe). I will blow your mind and ride you to oblivion. You will never forget me. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elit velit, convallis id consectetur ut, varius et diam. Nam lacus orci, elementum et bibendum ultricies, tincidunt nec lectus. Duis blandit et mi in sodales. Fusce nec purus porttitor, pharetra mi a, accumsan nisi. Integer neque urna, cursus sed ex id, auctor vehicula mi. Donec ac finibus magna. Proin sollicitudin erat sit amet orci tincidunt, facilisis lacinia dolor ornare.",
-      imageURL: "/images/horse.webp",
+      imageURL: "images/horse.webp",
     },
     {
       id: Math.random().toString().substring(0, 5),
